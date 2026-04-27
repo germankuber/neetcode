b56932e80301b7a639498671f377421d25a8fe2c
Germán Küber <61828554+germankuber@users.noreply.github.com>
2026-04-27 16:56:44 +0100

    Add: anagram-groups - submission-3

---
+++ Data Structures & Algorithms/anagram-groups/submission-3.py
@@ -0,0 +1,20 @@
+from typing import List
+
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        result:dict[tuple, list] = {}
+        for word in strs:
+            list_id = [0] * 26
+            
+            for letter in word:
+                list_id[ ord(letter) - ord("a")] += 1
+
+            id = tuple(list_id)
+            if id not in result:
+                result[id] = []
+            
+            result[id].append(word)
+                
+        
+        return list(result.values())
