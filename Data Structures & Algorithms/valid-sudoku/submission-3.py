8ff915a89c26093a6f70bf28c3be1d59d3bae136
Germán Küber <61828554+germankuber@users.noreply.github.com>
2026-04-28 14:25:32 +0100

    Add: valid-sudoku - submission-3

---
+++ Data Structures & Algorithms/valid-sudoku/submission-3.py
@@ -0,0 +1,34 @@
+from typing import List
+
+
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        rows = [[False] * 9 for _ in range(9)]
+        columns = [[False] * 9 for _ in range(9)]
+        quad = [[False] * 9 for _ in range(9)]
+        
+        for row_index, row in enumerate(board):
+            
+            for col_index, value in enumerate(row):
+                
+                if value != ".":
+                    if not rows[row_index][int(value) - 1]:
+                        rows[row_index][int(value) - 1] = True
+                    else:
+                        return False
+                    if not columns[col_index][int(value) - 1]:
+                        columns[col_index][int(value) - 1] = True
+                    else:
+                        return False
+                    quad_index = self.get_quad_index(col_index, row_index)
+                    if not quad[quad_index][int(value) - 1]:
+                        quad[quad_index][int(value) - 1] = True
+                    else:
+                        return False
+                
+                
+        return True
+    
+    def get_quad_index(self, col_index:int, row_index:int)-> int:
+        quad = int((row_index / 3)) * 3 + int(col_index / 3)
+        return  quad
