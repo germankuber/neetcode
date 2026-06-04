f2d8419aa478facddb815d589e6bdf6f2f602139
Germán Küber <61828554+germankuber@users.noreply.github.com>
2026-06-04 18:33:59 +0100

    Add: accounts-merge - submission-0

---
+++ Data Structures & Algorithms/accounts-merge/submission-0.py
@@ -0,0 +1,57 @@
+from collections import defaultdict
+from typing import List
+
+class UnionFind:
+    def __init__(self, size: int):
+        self.parent = [i for i in range(size)]
+
+    def find(self, node: int) -> int:
+        while node != self.parent[node]:
+            parent = self.parent[node]
+            grandparent = self.parent[parent]
+
+            self.parent[node] = grandparent
+
+            node = grandparent
+
+        return node
+
+    def union(self, node_a: int, node_b: int) -> bool:
+        root_a = self.find(node_a)
+        root_b = self.find(node_b)
+
+        if root_a == root_b:
+            return False
+
+        self.parent[root_a] = root_b
+        return True
+    
+    
+class Solution:
+    def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
+        union_find = UnionFind(len(accounts))
+        
+        email_to_account : dict[str, int] = {}
+        
+        for index, account in enumerate(accounts):
+            for  email in account[1:]:
+                if email in email_to_account:
+                    union_find.union(index, email_to_account[email])
+                else:
+                    email_to_account[email] = index
+                    
+        
+        email_group =  defaultdict(list)
+        
+        for email, index in email_to_account.items():
+            account_owner =  union_find.find(index)
+            email_group[account_owner].append(email)
+            
+        result = []
+        
+        for index, emails in email_group.items():
+            name = accounts[index][0]
+            result.append([name] + sorted(email_group[index]))
+            
+        return result
+        
